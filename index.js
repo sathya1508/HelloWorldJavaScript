5193c05f90ba431b036303d3de01015a1f2192b0
sathya1508 <sathya1508@gmail.com>
2021-10-25 21:41:24 +0530

    2 files modified

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+const btn = document.getElementById('buttonMe');
+btn.innerText = 'Hard';
+
+function ChangeColor ()
+{
+  var labelId = document.getElementById('lbl');
+  labelId.innerText.fontcolor = red;
+}
